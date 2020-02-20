07c4d3a44ad37895db32a17702146e3e2f57f987
Psh <dev@mtbdx.com>
2020-02-20 20:39:28 +0900

    Remove old search

--- controllers/search.rb
+++ controllers/search.rb
@@ -1,35 +1,4 @@
-path search_old: '/search_old',
-     search: '/search'
-
-get :search_old do
-  protect!
-
-  @words = []
-  @kanji = []
-  @radicals = []
-  @russian_words = RussianWord.none
-  q = params['query'].strip
-  @title = "🔎 #{q}"
-
-  if q.present?
-    if params[:japanese].present?
-      q_hiragana = q.downcase.hiragana
-
-      @words = Word.where('searchable_jp LIKE ? OR searchable_jp LIKE ?', "%#{q}%", "%#{q_hiragana}%").order(:is_common, :id).limit(1000).with_progresses(current_user).sort{|a,b| a.kreb_min_length <=> b.kreb_min_length}
-      @kanji = Kanji.where('kanji.title LIKE ?', q).order(:grade).with_progresses(current_user)
-    else
-      if q.length > 1
-        @russian_words = RussianWord.where("title ILIKE ?", "#{q}%").order(id: :asc)
-      end
-
-      @words = Word.where('searchable_en ILIKE ? OR searchable_ru ILIKE ?', "%#{q}%", "%#{q}%").order(:is_common, :id).limit(1000).with_progresses(current_user).sort{|a,b| a.kreb_min_length <=> b.kreb_min_length}
-      @kanji = Kanji.where('searchable_en ILIKE ?', "%#{q}%").order(:grade).with_progresses(current_user)
-      @radicals = WkRadical.where('meaning ILIKE ?', "%#{q}%").order(:level).with_progresses(current_user)
-    end
-  end
-
-  slim :search_old
-end
+paths search: '/search'
 
 get :search do
   protect!
--- src/main.js
+++ src/main.js
@@ -17,15 +17,3 @@ require('./vue-pitch-word-nhk.js');
 require('./vue-kanji-readings.js');
 require('./vue-settings-button.js');
 
-/* ===================
-   GLOBAL HOTKEYS
-   =================== */
-$(function(){
-  $('.old-search-form input.search').on('keyup', function(event){
-    if (event.which === 38 || event.which === 40) { // Up/Down keys
-      var el = $('.old-search-form input[name=japanese]');
-      el.prop('checked', el.is(':checked') ? false : true);
-    }
-  });
-});
-
