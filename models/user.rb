ffc1a0538253b495be6087a3a15f6955b7845850
Psh <dev@mtbdx.com>
2022-02-20 19:35:39 +0900

    Add missed relations to user model

--- models/user.rb
+++ models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
-  has_many :statistics
-  has_many :notes
-  has_many :progresses
+  has_many :activities, dependent: :destroy
+  has_many :drills, dependent: :destroy
+  has_many :notes, dependent: :destroy
+  has_many :progresses, dependent: :destroy
+  has_many :sentences, dependent: :destroy
+  has_many :sentence_reviews, dependent: :destroy
+  has_many :srs_progresses, dependent: :destroy
+  has_many :statistics, dependent: :destroy
+  has_many :word_details, dependent: :destroy
 
   def password=(pwd)
     self.salt = SecureRandom.hex
